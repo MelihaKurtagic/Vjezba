f7c25914f6da36592f7cdf219fcd57047f8e7e59
MelihaKurtagic <kurtagic.meliha@hotmail.com>
2017-04-23 20:54:45 +0200

    Add files via upload

---
+++ BookingDemo/src/main/java/code/booking/HotelBooking.java
@@ -0,0 +1,46 @@
+package code.booking;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class HotelBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private String hotelName;
+    private double pricePerNight;
+    private int numOfNights;
+
+    public HotelBooking(){}
+
+    public HotelBooking(String hotelName, double pricePerNight, int numOfNights) {
+        this.hotelName = hotelName;
+        this.pricePerNight = pricePerNight;
+        this.numOfNights = numOfNights;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public int getNumOfNights() {
+        return numOfNights;
+    }
+
+    public double getTotalPrice() {
+        return pricePerNight * numOfNights;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
